9c7ccdd12a40fb1e9bcf29145442e6b87f34b37e
Abhin Pai <abhinpai96@gmail.com>
2019-07-16 23:49:37 +0530

    Initial Commit

---
+++ lib/UI/HomePage.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+   HomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+    Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("UI Challenge"),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: RaisedButton(
+              child: Text("Challenge$index"),
+              onPressed: () {
+                Navigator.pushNamed(context, '/challenge$index');
+              },
+            ),
+          );
+        },
+        itemCount: 4,
+        shrinkWrap: true,
+      ),
+    );
+    // return Scaffold();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_challenge/UI/Challenge1/test.dart';
+import 'package:flutter_ui_challenge/UI/HomePage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // home: new HomePage(),
+      // initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/challenge0': (context) => Test()
+      },
+    );
+  }
+}
